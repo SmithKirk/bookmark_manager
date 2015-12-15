ec72aab501c37e6e3b40f4778b78e40f42cfcc0e
Kirk Smith <kirks@hotmail.co.uk>
2015-12-15 17:17:44 +0000

    10:creating modular sinatra app

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+
+ruby '2.2.3'
+
+gem 'rspec'
+gem 'capybara'
+gem 'sinatra'
+gem 'rspec-sinatra'
+gem 'dm-postgres-adapter'
+gem 'data_mapper'
+
+group :development do
+  gem 'byebug'
+end
---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,12 @@
+feature 'Viewing links' do
+  scenario 'see bookmarked links on homepage' do
+    Link.create(url: 'http://makersacademy.com', title: 'Makers Academy')
+
+    visit '/links'
+
+    expect(page.status_code).to eq 200 #quick check, remove later
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+  end
+end
