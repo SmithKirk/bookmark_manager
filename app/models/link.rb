9abc0128a0105e0e3f6cdbd27eaaddea9a871cb2
Kirk Smith <kirks@hotmail.co.uk>
2015-12-16 14:09:47 +0000

    13: config rack env

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'dm-migrations'
 
 class Link
 
@@ -10,6 +11,6 @@ class Link
   property :url, String
 end
 
-DataMapper.setup(:default,"postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default,"postgres://localhost/bookmark_manager_#{ENV["RACK_ENV"]}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
